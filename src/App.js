264b38834600d982d06b2cb18658474c73de0eb7
Origho-precious <origho9@gmail.com>
2021-08-21 02:22:39 +0100

    feat: setting app routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Homepage from "./pages/home";
+
 const App = () => {
-	return <div>App</div>;
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/" component={Homepage} />
+				<Route exact path="/anime/:name" component={Homepage} />
+			</Switch>
+		</BrowserRouter>
+	);
 };
 
 export default App;

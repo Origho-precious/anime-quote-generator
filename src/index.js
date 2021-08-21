3971321b3203cf1f2af0fc9f8f64685e6b31f168
Origho-precious <origho9@gmail.com>
2021-08-21 02:41:54 +0100

    feat: adding recoil and setting root config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { RecoilRoot } from "recoil";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<RecoilRoot>
+		<App />
+	</RecoilRoot>,
+	document.getElementById("root")
 );

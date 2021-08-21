4421fa0d2bcf22c96c6d3eef6827b978d72af341
Origho-precious <origho9@gmail.com>
2021-08-21 02:23:09 +0100

    feat: adding homepage

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+import AnimePill from "./components/AnimePill/AnimePill";
+import Quote from "./components/Quote/Quote";
+
+const Homepage = () => {
+	const colors = ["#FAE1DA", "#E8C6AD", "#F2E2ED", "#D6EBE4", "#BFDCD0"];
+	const animes = [
+		"Naruto",
+		"Hellsing",
+		"Naruto",
+		"Hellsing",
+		"Naruto",
+		"Hellsing",
+		"Naruto",
+		"Hellsing",
+	];
+
+	const generateColor = () => {
+		const randNum = Math.floor(Math.random() * 5);
+		return colors[randNum];
+	};
+
+	return (
+		<StyledHomePage>
+			<header>
+				<h2>Anime Quote Generator</h2>
+			</header>
+			<main>
+				<Quote />
+				<div className="animes">
+					<h3>All Animes</h3>
+					<p>Click on any anime to see a quote from it</p>
+					<div className="flex">
+						{animes?.map((anime) => (
+							<div style={{ margin: "0 1.2rem 1.2rem 0" }}>
+								<AnimePill anime={anime} color={generateColor()} />
+							</div>
+						))}
+					</div>
+				</div>
+			</main>
+		</StyledHomePage>
+	);
+};
+
+const StyledHomePage = styled.div`
+	max-width: 80%;
+	margin: 2rem auto;
+
+	& header {
+		margin-bottom: 3rem;
+
+		& > h2 {
+			font-weight: 400;
+			letter-spacing: 3px;
+			text-align: center;
+		}
+	}
+
+	& .animes {
+		margin-top: 4rem;
+
+		& h3 {
+			font-weight: 400;
+			font-size: 1.4rem;
+			background: #ece4f1;
+			width: max-content;
+			padding: 0.3rem 1rem;
+		}
+		& p {
+			margin: 1rem 0;
+		}
+
+		& .flex {
+			display: flex;
+			flex-wrap: wrap;
+		}
+	}
+`;
+
+export default Homepage;
